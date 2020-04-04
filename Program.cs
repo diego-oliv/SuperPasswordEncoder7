e88346ae2b9eed01427521bdc3831b520d65d5f2
lspetersen <trezum@gmail.com>
2020-04-04 22:58:05 +0200

    Use hashing for passwordgeneration, more indput characters supported but only base64 output supported. Added a variable for desired password length but no console interface for setting it.

--- Program.cs
+++ Program.cs
@@ -1,224 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SPE7
 {
     class Program
     {
+        // This variable is needed to increase performance for long passwords.
+        private static string latestHash = "";
         static void Main(string[] args)
         {
             Console.Clear();
-            string password = "";
-            string codifiedPassword = "";
+            int desiredPasswordLength = 100000;
 
             Intro();
-            System.Console.Write("Type the word to encrypt it: ");
-            password = Console.ReadLine().ToLower();
+            Console.Write("Type the word to encrypt it: ");
+            string password = Console.ReadLine();
 
-            List<String> passwordChars = new List<string>();
-            char[] passwordArray = password.ToCharArray();
-            for(int i=0; i < passwordArray.Length; i++)
+            string codifiedPassword = ComputeHashForString(password);
+            while (codifiedPassword.Length < desiredPasswordLength)
             {
-                switch(passwordArray[i])
-                {
-                    case 'a':
-                    string charA = "U8tHtVi8Gk9";
-                    passwordChars.Add(charA);
-                    break;
-
-                    case 'b':
-                    string charB = "xmRSC5aHC$@";
-                    passwordChars.Add(charB);
-                    break;
-
-                    case 'c':
-                    string charC = "rQ8jGSd%vRT";
-                    passwordChars.Add(charC);
-                    break;
-
-                    case 'd':
-                    string charD = "Vjv0$Xwta";
-                    passwordChars.Add(charD);
-                    break;
-
-                    case 'e':
-                    string charE = "9JCsln";
-                    passwordChars.Add(charE);
-                    break;
-
-                    case 'f':
-                    string charF = "yoWWVib570g";
-                    passwordChars.Add(charF);
-                    break;
-
-                    case 'g':
-                    string charG = "XwtaTbCeq";
-                    passwordChars.Add(charG);
-                    break;
-
-                    case 'h':
-                    string charH = "f@M2HuUW";
-                    passwordChars.Add(charH);
-                    break;
-
-                    case 'i':
-                    string charI = "NfBNfsSjp3Qu@";
-                    passwordChars.Add(charI);
-                    break;
-
-                    case 'j':
-                    string charJ = "FxRwldWapXG#";
-                    passwordChars.Add(charJ);
-                    break;
-
-                    case 'k':
-                    string charK = "qS$aTMTldkY";
-                    passwordChars.Add(charK);
-                    break;
-
-                    case 'l':
-                    string charL = "MaIdLkT50C3R";
-                    passwordChars.Add(charL);
-                    break;
-
-                    case 'm':
-                    string charM = "HG%$iO0wiiwoQ%%b";
-                    passwordChars.Add(charM);
-                    break;
-
-                    case 'n':
-                    string charN = "5b9T7bKxs";
-                    passwordChars.Add(charN);
-                    break;
-
-                    case 'o':
-                    string charO = "sO0nu";
-                    passwordChars.Add(charO);
-                    break;
-
-                    case 'p':
-                    string charP = "umIXbCZ80";
-                    passwordChars.Add(charP);
-                    break;
-
-                    case 'q':
-                    string charQ = "Ztx7ljuJ";
-                    passwordChars.Add(charQ);
-                    break;
-
-                    case 'r':
-                    string charR = "s#mDl3bl%";
-                    passwordChars.Add(charR);
-                    break;
-
-                    case 's':
-                    string charS = "2FOP%XN1Jw";
-                    passwordChars.Add(charS);
-                    break;
-
-                    case 't':
-                    string charT = "UPJPZ#J0l";
-                    passwordChars.Add(charT);
-                    break;
-
-                    case 'u':
-                    string charU = "cDLrZDoiAcv";
-                    passwordChars.Add(charU);
-                    break;
-
-                    case 'v':
-                    string charV = "KZnwpn2yZ9mdl@UeW";
-                    passwordChars.Add(charV);
-                    break;
-
-                    case 'w':
-                    string charW = "sfq%1bhCuKp%";
-                    passwordChars.Add(charW);
-                    break;
-
-                    case 'x':
-                    string charX = "%aMhEG2kI2";
-                    passwordChars.Add(charX);
-                    break;
-
-                    case 'y':
-                    string charY = "baw7G%k6ddX5";
-                    passwordChars.Add(charY);
-                    break;
-
-                    case 'z':
-                    string charZ = "@DhFjTC%sm0cISa";
-                    passwordChars.Add(charZ);
-                    break;
-
-                    case '0':
-                    string char0 = "eNZ4ZnUH02h";
-                    passwordChars.Add(char0);
-                    break;
-
-                    case '1':
-                    string char1 = "nmbcwHgF38%p";
-                    passwordChars.Add(char1);
-                    break;
-
-                    case '2':
-                    string char2 = "mRJP9bbgOQT";
-                    passwordChars.Add(char2);
-                    break;
-
-                    case '3':
-                    string char3 = "Kko4LgQpwBpF";
-                    passwordChars.Add(char3);
-                    break;
-
-                    case '4':
-                    string char4 = "hWkxl";
-                    passwordChars.Add(char4);
-                    break;
-
-                    case '5':
-                    string char5 = "tKO8Ot8RW";
-                    passwordChars.Add(char5);
-                    break;
-
-                    case '6':
-                    string char6 = "Ui966BPBBbyd";
-                    passwordChars.Add(char6);
-                    break;
-
-                    case '7':
-                    string char7 = "fmsp0jDU$IEF4iPI";
-                    passwordChars.Add(char7);
-                    break;
-
-                    case '8':
-                    string char8 = "$5h0v7HKbHkA";
-                    passwordChars.Add(char8);
-                    break;
-
-                    case '9':
-                    string char9 = "Aq$cEV%5D6o";
-                    passwordChars.Add(char9);
-                    break;
-                }
+                codifiedPassword += ComputeHashForString(latestHash);
             }
+            codifiedPassword = codifiedPassword.Substring(0, desiredPasswordLength);
 
-            foreach(string passwordChar in passwordChars)
-            {
-                codifiedPassword += passwordChar;
-            }
             int passwordLenght = codifiedPassword.Length;
-            System.Console.Write("This is your encrypted password: ");
-            System.Console.WriteLine(codifiedPassword);
-            System.Console.WriteLine($"The password has {passwordLenght} characters.");
+            Console.Write("This is your encrypted password: ");
+            Console.WriteLine(codifiedPassword);
+            Console.WriteLine($"The password has {passwordLenght} characters.");
+        }
+
+        private static string ComputeHashForString(string stringToHash)
+        {
+            using (SHA256 mySHA256 = SHA256.Create())
+            {
+                var hash = mySHA256.ComputeHash(Encoding.UTF8.GetBytes(stringToHash));
+                latestHash = Convert.ToBase64String(hash);
+                return latestHash;
+            }
         }
 
         private static void Intro()
         {
-            System.Console.WriteLine("======================================");
-            System.Console.WriteLine("      Super Passoword Encoder 7");
-            System.Console.WriteLine("======================================");
-            System.Console.WriteLine();
+            Console.WriteLine("======================================");
+            Console.WriteLine("      Super Passoword Encoder 7");
+            Console.WriteLine("======================================");
+            Console.WriteLine();
         }
     }
 }
